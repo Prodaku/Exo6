e0c1f8be39698acd48c16bf17b417e7d0123f5e0
Prodaku <b.isaacpro@gmail.com>
2018-12-18 09:56:55 +0100

    Fini

---
+++ Exo6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entrez votre NOM.");
+            string Name = Console.ReadLine();
+            Console.WriteLine("Entrez votre PRENOM.");
+            string FirstName = Console.ReadLine();
+
+            Console.WriteLine("Entrez votre ANNEE DE NAISSANCE.");
+            int DOB = int.Parse(Console.ReadLine()); //DateOfBirth
+
+            string LJ = "\n"; //LineJump
+
+            int age = DateTime.Now.Year - DOB;
+
+            Console.WriteLine("Fiche de renseignement " + LJ + LJ + "Nom : " + Name + LJ + "Prénom : " 
+                + FirstName + LJ + "Age : " + age + LJ);
+
+            
+        }
+    }
+}
